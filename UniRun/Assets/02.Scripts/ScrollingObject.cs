235cc962d0584c2c8570c4dd8e05032bcc1194c4
JeongSeongYong <99639179+JeongSeongYong@users.noreply.github.com>
2022-03-31 14:52:17 +0900

    Uniruncopy

--- UniRun/Assets/02.Scripts/ScrollingObject.cs
+++ UniRun/Assets/02.Scripts/ScrollingObject.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 // 게임 오브젝트를 지속적으로 왼쪽으로 움직이는 스크립트
 public class ScrollingObject : MonoBehaviour
 {
-    public float speed = 10f; // 이동 속도
+    public float speed = 3f; // 이동 속도
 
     void Update()
 
